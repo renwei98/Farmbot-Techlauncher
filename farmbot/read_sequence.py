719c67f609b4b22577c8daa8ac11f017bbaec842
Zhixian Wu <u5807060@anu.edu.au>
2019-03-27 16:00:14 +1100

    changed function signatures, considering design patterns

--- farmbot/read_sequence.py
+++ farmbot/read_sequence.py
@@ -3,32 +3,55 @@ import subscribe_to_farmbot.client as client
 import subscribe_to_farmbot.my_device_id as my_device_id
 
 class SequenceHander():
-    def __init__(self,  yaml_file_name, csv_file_name):
-        self.sequences = {}
-        self.file = open(file_name, "r")
+    def __init__(self,  yaml_file_names, csv_file_names):
+        """yaml_file_names : a list of YAML files holding sequences
+           csv_file_names  : a list of CSV files holding map coordinates"""
+        self.seq_files = {} # {source file : yaml object}
+        self.celeryscript = {} # {sequence name : celeryscript}
+        self.seq_files = set(yaml_file_names)
+        self.map = set(csv_file_names)
+        self.load_sequences()
 
-    def load_sequences(self, yaml_file_name, csv_file_name):
-        """sequences : A dictionary of sequences, by "name or ID", to be stored as CeleryScript
-           file_name : Name of the file the YAML sequence is stored in."""
-        # Note: the "ID" is only obtained after sending it to FarmBot
-        # So it will need to have a placeholder name until we get an ID for it
-        file = open(file_name, "r")
-        yaml_file = yaml.load(file)
-        # Convert YAML to CeleryScript here
-        for name in sequences:
-            cs_obj = convert_sequence(yaml_file[name])
-            client.publish("bot/" + my_device_id + "/from_clients", json_payload)
+    def load_sequences(self):
+        """Modifies self.seq_files and self.celeryscript"""
+        for source in self.seq_files:
+            file = open(source, "r")
+            yaml_file = yaml.load(file)
+            self.seq_files[source] = yaml_file
+            # Convert YAML to CeleryScript here
+            for node in yaml_file:
+                if node["kind"]=="sequence":
+                    if node["name"] not in self.sequences:
+                        self.celeryscript[node["name"]] = (convert_sequence(yaml_file[name]), source)
+                    else:
+                        print("Placeholder message for duplicate sequences")
 
     def convert_sequence(yaml_obj):
         """yaml_obj: A YAML object representing a sequence
            returns: A CeleryScript JSON object of a sequence."""
-           # This function can call itself to return well-formatted sub-sequences
+           # This function will need to refer to sub-sequences by name
+           # and send them first until you get an id back
+
+    # ** PATRICK TAKE NOTE **
+    # Call this after sending a sequence, so we can store the returned ID
+    def update_sequence(self, seq_name, returned_id):
+        source = self.celeryscript[seq_name][1]
+        self.seq_files[source][seq_name]["id"] = returned_id
+        yaml.dump(self.seq_files[source], source)
+
+    def add_seq_source(self, yaml_file):
+        """Adds another YAML source file to the SequenceHandler."""
+        self.seq_files.add(yaml_file)
+
+    def add_map_source(self, csv_file):
+        """Adds another map source file to the SequenceHandler."""
+        self.map.add(csv_file)
 
     def example_sequence():
         """This is for the Regimen code writer to use. From a SequenceHander
         object it returns a dictionary with two named Sequences for them to use.
         The -if-then-else uses an existing sequence (ID: 2) loaded on the FarmBot"""
-        return {"example" : """
+        return {"Example 1" : """
         {
   "kind": "sequence",
   "name": "Example 1",
