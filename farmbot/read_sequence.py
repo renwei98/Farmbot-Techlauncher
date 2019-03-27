f6d6fb5aee8e75a2d204eb1cc5d954b2024c5f06
Zhixian Wu <u5807060@anu.edu.au>
2019-03-27 11:35:07 +1100

    returns example sequence

--- farmbot/read_sequence.py
+++ farmbot/read_sequence.py
@@ -3,13 +3,15 @@ import subscribe_to_farmbot.client as client
 import subscribe_to_farmbot.my_device_id as my_device_id
 
 class SequenceHander():
-    def __init__(self,  file_name):
-        self.sequences = []
+    def __init__(self,  yaml_file_name, csv_file_name):
+        self.sequences = {}
         self.file = open(file_name, "r")
 
-    def send_sequences(sequences, file_name):
-        """sequences : A list of sequences, by "name", to be sent to the FarmBot
+    def load_sequences(self, yaml_file_name, csv_file_name):
+        """sequences : A dictionary of sequences, by "name or ID", to be stored as CeleryScript
            file_name : Name of the file the YAML sequence is stored in."""
+        # Note: the "ID" is only obtained after sending it to FarmBot
+        # So it will need to have a placeholder name until we get an ID for it
         file = open(file_name, "r")
         yaml_file = yaml.load(file)
         # Convert YAML to CeleryScript here
@@ -21,3 +23,44 @@ class SequenceHander():
         """yaml_obj: A YAML object representing a sequence
            returns: A CeleryScript JSON object of a sequence."""
            # This function can call itself to return well-formatted sub-sequences
+
+    def example_sequence():
+        """This is for the Regimen code writer to use. From a SequenceHander
+        object it returns a dictionary with two named Sequences for them to use.
+        The -if-then-else uses an existing sequence (ID: 2) loaded on the FarmBot"""
+        return {"example" : """
+        {
+  "kind": "sequence",
+  "name": "Example 1",
+  "color": "gray",
+
+  },
+  "body": [
+    {
+      "kind": "_if",
+      "args": {
+        "lhs": "pin1",
+        "op": "is",
+        "rhs": 1,
+        "_then": {
+          "kind": "execute",
+          "args": {
+            "sequence_id": 2
+          }
+        },
+        // Else branch
+        "_else": {
+          "kind": "nothing",
+          "args": {
+          }
+        }
+      }
+    },
+    {
+      "kind": "wait",
+      "args": {
+        "milliseconds": 2300
+      }
+    }
+  ]
+}"""}
