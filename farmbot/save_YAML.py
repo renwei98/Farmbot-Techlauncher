f5f39dbd32a730402e0564b05a1b06981c67bc30
Wei <9812renwei@gmail.com>
2019-04-18 00:44:05 +1000

    Save to local machine with josn

--- farmbot/save_YAML.py
+++ farmbot/save_YAML.py
@@ -2,9 +2,15 @@
 """
 Created on Wed Apr 17 23:34:05 2019
 
-@author: Mr. R
+@author: Wei
 """
+import yaml, json, sys
 
-file_handle=open("file.txt", 'w+')
-file_handle.write("Test")
+file_read = open("file.json", 'r')
+if f.mode == 'r':
+    contents = file_read.read()
+    #contents = sys.stdout.write(yaml.dump(json.load(sys.stdin)))
+
+file_handle = open("file.yaml", 'w+')
+file_handle.write(contents)
 file_handle.close()
