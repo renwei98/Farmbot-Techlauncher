70bf9c40e4bd30aeca479fa074fb5ef3d064bf0e
zizheng huang <huangzizheng98@gmail.com>
2019-04-18 01:12:11 +1000

    Update save_YAML.py

--- farmbot/save_YAML.py
+++ farmbot/save_YAML.py
@@ -1,5 +1,13 @@
-
 import yaml, json, sys
+import requests
+import os
+
+headers = {'Authorization': 'Bearer ' + os.getenv("API_KEY"), 'content-type': 'application/json'}
+logs = requests.get('https://my.farmbot.io/api/logs', headers=headers)
+
+
+file_handle=open('file.json',mode='w')
+file_handle.write(logs.json())
 
 file_read = open("file.json", 'r')
 if f.mode == 'r':
