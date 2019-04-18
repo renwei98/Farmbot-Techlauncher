33d7d67b6d5c90faf62a466ff8530f155d41d05a
zizheng huang <huangzizheng98@gmail.com>
2019-04-18 10:57:10 +1000

    Update csv_to_script.py

--- farmbot/csv_to_script.py
+++ farmbot/csv_to_script.py
@@ -3,9 +3,10 @@ import pandas as pd
 def csv_to_celeryscript(path):
     content = pd.read_csv(path)
     n = 0
+    script = ""
     while n <= len(content):
         
-        script = ""
+        
         script = script + "\n  \"plant_type\" : \"" + content["plant_type"][n] + "\","
         script = script + "\n  \"group_number\" : " + content["group_number"][n] + "\","
         script = script + "\n  \"x_coordinates\" : \"" + content["x_coordinates"][n] + "\","
