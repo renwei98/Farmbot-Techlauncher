efe37d95b8f0f32c4de3627c155b11d3658e6142
u6203160 <gwjinori@foxmail.com>
2019-04-19 18:26:01 +1000

    update

--- farmbot/status_check_change.py
+++ farmbot/status_check_change.py
@@ -1,20 +1,26 @@
 import pandas as pd
-
-    def origin_valu(value):
-        data = pd.read_csv(path)
-        x = data.x_coordinates
-        y = data.y_coordinates 
-        z = data.z_coordinates
+    data = pd.read_csv(path)
+    x = data.x_coordinates
+    y = data.y_coordinates 
+    z = data.z_coordinates
 
     def excuted_value(regimen):
-        if self.value != new_value:
+        for elemx in x:
+            if elemx != new_value:
             self.pre_change()
-        else:
+            else:
             self.post_change()
-
+        for elemy in y:
+            if elemy != new_value:
+            self.pre_change()
+            else:
+            self.post_change()
+        for elemz in z:
+            if elemz != new_value:
+            self.pre_change()
+            else:
+            self.post_change()
     def pre_change(self):
-        # do stuff before variable is about to be changed
         print (False)
     def post_change(self):
-        # do stuff right after variable has changed
         print (True)
