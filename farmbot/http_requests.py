22e74ae3e29b5d296b00b45caea869b0039d5f50
patrick@iisys.com.au <8S35RsFyhg0A>
2019-04-30 02:59:03 +1000

    Revert "Merge branch 'master' of https://github.com/dopfer/Farmbot-Techlauncher"
    This reverts commit db5f3fe92fb41200d14f7d30eec3bfa66e6007cb, reversing
    changes made to 5d4d9bdfd3892edfaf6c70b797be6b866ea4f4c3.

--- farmbot/http_requests.py
+++ farmbot/http_requests.py
@@ -1,39 +1,57 @@
 import requests
 from dotenv import load_dotenv
 import os
-from enum import Enum
-
-
-class Event(Enum):
-    REGIMEN = 'regimen'
-    SEQUENCE = 'sequence'
-    FARM_EVENT = 'farm_event'
-
 
 load_dotenv()  # Add API_KEY to .env if not done already
 headers = {'Authorization': 'Bearer ' + os.getenv("API_KEY"), 'content-type': 'application/json'}
 
 # Get all sequences currently on Farmbot
-def get_sequences():  # TODO be able to get regimens and farm events too
-    sequences = requests.get('https://my.farmbot.io/api/sequences', headers=headers)
-    return sequences.json()
+sequences = requests.get('https://my.farmbot.io/api/sequences', headers=headers)
+print(sequences.json())
 
+# Create new example sequence
+json = {
+  "name": "EXAMPLE SEQUENCE",
+  "body": [
+    {
+      "kind": "move_absolute",
+      "args": {
+        "location": {
+          "kind": "coordinate",
+          "args": {
+            "x": 1,
+            "y": 2,
+            "z": 3
+          }
+        },
+        "offset": {
+            "kind": "coordinate",
+            "args": {
+                "x": 0,
+                "y": 0,
+                "z": 0
+            }
+          },
+        "speed": 4
+      }
+    }]
+}
+
+print(type(json))
 
 # Sends CeleryScript objects to FarmBot and returns the ID.
-def new_sequence(json_script, event_name: str, event_type: Event):  # TODO rename if sequence, regimen, event uses this
+def get_id_back(json_script, name=None):
     id = -1
-    if event_type == Event.FARM_EVENT:
+    kind = json_script["kind"]
+    if "start_time" in json_script:
         new_item = requests.post('https://my.farmbot.io/api/farm_events', headers=headers, json=json_script)
-        id = new_item.json()["id"]
-    elif event_type == Event.REGIMEN:
+        id = new_item["id"]
+    elif "regimen_items" in json_script:
         new_item = requests.post('https://my.farmbot.io/api/regimens', headers=headers, json=json_script)
-        id = new_item.json()["id"]
-    elif event_type == Event.SEQUENCE:
+        id = new_item["id"]
+    else:
         new_item = requests.post('https://my.farmbot.io/api/sequences', headers=headers, json=json_script)
-        id = new_item.json()["id"]
-
-    return id
-
+        id = new_item["id"]
 
 # Tells FarmBot to delete something
 def delete_command(id, kind):
@@ -43,3 +61,12 @@ def delete_command(id, kind):
         requests.delete('https://my.farmbot.io/api/regimens/' + str(id), headers=headers)
     elif kind == "sequence":
         requests.delete('https://my.farmbot.io/api/sequences/' + str(id), headers=headers)
+
+new_sequence = requests.post('https://my.farmbot.io/api/sequences', headers=headers, json=json)
+sequence_id = new_sequence.json()['id']
+print(new_sequence.json())
+print("Sequence ID: ", sequence_id)
+
+# Delete newly created example sequence
+delete_sequence = requests.delete('https://my.farmbot.io/api/sequences/' + str(sequence_id), headers=headers)
+print(delete_sequence, "Sequence:", sequence_id, "Deleted")
