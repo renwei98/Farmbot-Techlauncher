a2a3aada930601fe767fcfc350e2293b10fa37e5
u6233682 <u6233682@anu.edu.au>
2019-05-02 22:11:08 +1000

    add iteration

--- farmbot/csv_to_script.py
+++ farmbot/csv_to_script.py
@@ -1,12 +1,18 @@
 import pandas as pd
+import json
 
-content = pd.read_csv(path)
-script = ""
-script1 = script + "\n  \"plant_type\" : \"" + content["plant_type"][n] + "\","
-script2 = script + "\n  \"group_number\" : \"" + (str(content["group_number"][n])) + "\","
-script3 = script + "\n  \"x_coordinates\" : \"" + (str(content["x_coordinates"][n])) + "\","
-script4 = script + "\n  \"y_coordinates\" : \"" + (str(content["y_coordinate"][n])) + "\","
-script5 = script + "\n  \"z_coordinates\" : \"" + (str(content["z_coordinate"][n])) + "\","
-script6 = script + "\n  \"init_plant_date\" : \"" + (str(content["init_plant_date"][n])) + "\","
-print("{", script1, script2, script3, script4, script5, script6,"\n}")
-        
+def csv_to_celeryscript(path):
+    content = pd.read_csv(path)
+    script = "{"
+    n = 0
+    while n <= len(content)-1:
+        script = script + "\n  \"plant_type\" : \"" + content["plant_type"][n] + "\","
+        script = script + "\n  \"group_number\" : \"" + (str(content["group_number"][n])) + "\","
+        script = script + "\n  \"x_coordinates\" : \"" + (str(content["x_coordinates"][n])) + "\","
+        script = script + "\n  \"y_coordinates\" : \"" + (str(content["y_coordinate"][n])) + "\","
+        script = script + "\n  \"z_coordinates\" : \"" + (str(content["z_coordinate"][n])) + "\","
+        script = script + "\n  \"init_plant_date\" : \"" + (str(content["init_plant_date"][n])) + "\",\n"
+        n+=1
+    script = script + "}"
+    print(script)
+    return
