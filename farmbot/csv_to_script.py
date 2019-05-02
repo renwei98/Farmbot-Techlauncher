42539f56aea5fe65150f30e5c480c50efbde4db2
u6233682 <u6233682@anu.edu.au>
2019-05-02 21:32:00 +1000

    read csv and convert it to celeryscript

--- farmbot/csv_to_script.py
+++ farmbot/csv_to_script.py
@@ -1,18 +1,12 @@
 import pandas as pd
 
-def csv_to_celeryscript(path):
-    content = pd.read_csv(path)
-    n = 0
-    script = ""
-    while n <= len(content):
+content = pd.read_csv(path)
+script = ""
+script1 = script + "\n  \"plant_type\" : \"" + content["plant_type"][n] + "\","
+script2 = script + "\n  \"group_number\" : \"" + (str(content["group_number"][n])) + "\","
+script3 = script + "\n  \"x_coordinates\" : \"" + (str(content["x_coordinates"][n])) + "\","
+script4 = script + "\n  \"y_coordinates\" : \"" + (str(content["y_coordinate"][n])) + "\","
+script5 = script + "\n  \"z_coordinates\" : \"" + (str(content["z_coordinate"][n])) + "\","
+script6 = script + "\n  \"init_plant_date\" : \"" + (str(content["init_plant_date"][n])) + "\","
+print("{", script1, script2, script3, script4, script5, script6,"\n}")
         
-        
-        script = script + "\n  \"plant_type\" : \"" + content["plant_type"][n] + "\","
-        script = script + "\n  \"group_number\" : " + content["group_number"][n] + "\","
-        script = script + "\n  \"x_coordinates\" : \"" + content["x_coordinates"][n] + "\","
-        script = script + "\n  \"y_coordinates\" : \"" + content["y_coordinates"][n] + "\","
-        script = script + "\n  \"z_coordinates\" : " + content["z_coordinates"][n] + "\","
-        script = script + "\n  \"init_plant_date\" : \"" + content["init_plant_date"][n] + "\","
-        n += 1
-        
-    return script
