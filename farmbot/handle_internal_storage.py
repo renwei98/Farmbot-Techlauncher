d5694c1a544636c4913bb7c7b0d13bac4a5fc2f4
Wei <9812renwei@gmail.com>
2019-05-02 13:52:24 +1000

    Update handle_internal_storage.py

--- farmbot/handle_internal_storage.py
+++ farmbot/handle_internal_storage.py
@@ -5,15 +5,15 @@ import os
 headers = {'Authorization': 'Bearer ' + os.getenv("API_KEY"), 'content-type': 'application/json'}
 logs = requests.get('https://my.farmbot.io/api/logs', headers=headers)
 
-
+#Open a file named file.json or create one if it doesn't exist.
 file_handle=open('file.json',mode='w')
 file_handle.write(logs.json())
 
+#Read file.json and transfer it to YAML.
 file_read = open("file.json", 'r')
 if file_read.mode == 'r':
     contents = file_read.read()
     contents = yaml.dump(json.load(contents))
-
 file_handle2 = open("file.yaml", 'w+')
 file_handle2.write(contents)
 file_handle2.close()
