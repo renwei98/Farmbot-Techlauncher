7ab9c84d6e69d9ea55e6bf5d204f9d24729d90e4
zizheng huang <huangzizheng98@gmail.com>
2019-05-03 19:42:08 +1000

    csv to script using dictionary

--- farmbot/csv_to_script.py
+++ farmbot/csv_to_script.py
@@ -1,17 +1,13 @@
 import pandas as pd
+import json
 
 def csv_to_celeryscript(path):
     content = pd.read_csv(path)
-    script = "{"
-    n = 0
-    while n <= len(content)-1:
-        script = script + "\n  \"name\" : \"" + (str(content["name"][n])) + "\","
-        script = script + "\n  \"type\" : \"" + (str(content["type"][n])) + "\","
-        script = script + "\n  \"group\" : \"" + (str(content["group"][n])) + "\","
-        script = script + "\n  \"x\" : \"" + (str(content["x"][n])) + "\","
-        script = script + "\n  \"y\" : \"" + (str(content["y"][n])) + "\","
-        script = script + "\n  \"z\" : \"" + (str(content["z"][n])) + "\",\n"
-        n+=1
-    script = script + "}"
+    script = content.to_dict(orient="records")
+    script = str(script)
+    script = script.replace("[","")
+    script = script.replace("]","")
+    script = script.replace("'",'"')
     
+    json.dumps(script)
     return script
