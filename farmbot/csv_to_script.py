1de12c673ab2f867a55d86dd6ac363fc5fc8f74c
u6233682 <u6233682@anu.edu.au>
2019-05-03 23:18:18 +1000

    test csv update again

--- farmbot/csv_to_script.py
+++ farmbot/csv_to_script.py
@@ -1,18 +1,6 @@
 import pandas as pd
 import json
 
-def csv_to_celeryscript(path):
-    content = pd.read_csv(path)
-    script = "{"
-    n = 0
-    while n <= len(content)-1:
-        script = script + "\n  \"plant_type\" : \"" + content["plant_type"][n] + "\","
-        script = script + "\n  \"group_number\" : \"" + (str(content["group_number"][n])) + "\","
-        script = script + "\n  \"x_coordinates\" : \"" + (str(content["x_coordinates"][n])) + "\","
-        script = script + "\n  \"y_coordinates\" : \"" + (str(content["y_coordinate"][n])) + "\","
-        script = script + "\n  \"z_coordinates\" : \"" + (str(content["z_coordinate"][n])) + "\","
-        script = script + "\n  \"init_plant_date\" : \"" + (str(content["init_plant_date"][n])) + "\",\n"
-        n+=1
-    script = script + "}"
-    print(script)
-    return
+content = pd.read_csv("test.csv")
+
+print(content)
