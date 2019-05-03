41b2ae70202d02d14cda2bdfbce2db491ebe1c21
zizheng huang <huangzizheng98@gmail.com>
2019-05-03 23:24:08 +1000

    add default value

--- farmbot/csv_to_script.py
+++ farmbot/csv_to_script.py
@@ -1,19 +1,24 @@
 import pandas as pd
-import json
 
 
-content = pd.read_csv("test.csv")
+content = pd.read_csv('test.csv')
 
-print(content)
-
-def csv_to_celeryscript(path):
-    content = pd.read_csv(path)
-    script = content.to_dict(orient="records")
-    script = str(script)
-    script = script.replace("[","")
-    script = script.replace("]","")
-    script = script.replace("'",'"')
+content['name'] = content['name'].fillna('plant')
+content['type'] = content['type'].fillna('extra_water_group')
+content['group'] = content['group'].fillna('radish')
+content['x'] = content['x'].fillna(str(0))
+content['y'] = content['y'].fillna(str(0))
+content['z'] = content['z'].fillna(str(0))
     
-    json.dumps(script)
-    return script
-
+script = "{"
+n = 0
+while n <= len(content)-1:
+    script = script + "\"name\" : \"" + (str(content["name"][n])) + "\","
+    script = script + "\"type\" : \"" + (str(content["type"][n])) + "\","
+    script = script + "\"group\" : \"" + (str(content["group"][n])) + "\","
+    script = script + "\"x\" : \"" + (str(content["x"][n])) + "\","
+    script = script + "\"y\" : \"" + (str(content["y"][n])) + "\","
+    script = script + "\"z\" : \"" + (str(content["z"][n])) + "\","
+    n+=1
+script = script + "}"
+    
