9c9456562a236b55537610ca03d9c33eb11df141
zizheng huang <huangzizheng98@gmail.com>
2019-05-03 19:03:11 +1000

    Update csv_to_script.py

--- farmbot/csv_to_script.py
+++ farmbot/csv_to_script.py
@@ -1,17 +1,16 @@
 import pandas as pd
-import json
 
 def csv_to_celeryscript(path):
     content = pd.read_csv(path)
     script = "{"
     n = 0
     while n <= len(content)-1:
-        script = script + "\n  \"plant_type\" : \"" + content["plant_type"][n] + "\","
-        script = script + "\n  \"group_number\" : \"" + (str(content["group_number"][n])) + "\","
-        script = script + "\n  \"x_coordinates\" : \"" + (str(content["x_coordinates"][n])) + "\","
-        script = script + "\n  \"y_coordinates\" : \"" + (str(content["y_coordinate"][n])) + "\","
-        script = script + "\n  \"z_coordinates\" : \"" + (str(content["z_coordinate"][n])) + "\","
-        script = script + "\n  \"init_plant_date\" : \"" + (str(content["init_plant_date"][n])) + "\",\n"
+        script = script + "\n  \"name\" : \"" + (str(content["name"][n])) + "\","
+        script = script + "\n  \"type\" : \"" + (str(content["type"][n])) + "\","
+        script = script + "\n  \"group\" : \"" + (str(content["group"][n])) + "\","
+        script = script + "\n  \"x\" : \"" + (str(content["x"][n])) + "\","
+        script = script + "\n  \"y\" : \"" + (str(content["y"][n])) + "\","
+        script = script + "\n  \"z\" : \"" + (str(content["z"][n])) + "\",\n"
         n+=1
     script = script + "}"
     print(script)
