004a0026c80a1e5014c062666980c14d62ac03e9
Wei  Ren <u6233682@anu.edu.au>
2019-05-04 18:55:05 +1000

    update csv to script

--- farmbot/csv_to_script.py
+++ farmbot/csv_to_script.py
@@ -1,24 +1,31 @@
 import pandas as pd
-
+import numpy as np
 
 content = pd.read_csv('../data/test.csv')
 
-content['name'] = content['name'].fillna('plant')
+def is_missing(type, group):
+    if pd.isnull(type):
+        return content.loc[content['group'] == group, 'type']
+    else:
+        return type
+content['type'] = content.apply(lambda x : is_missing(x['type'], x['group']), axis = 1)
+
+"""content['name'] = content['name'].fillna('plant')
 content['type'] = content['type'].fillna('extra_water_group')
 content['group'] = content['group'].fillna('radish')
 content['x'] = content['x'].fillna(str(0))
 content['y'] = content['y'].fillna(str(0))
-content['z'] = content['z'].fillna(str(0))
+content['z'] = content['z'].fillna(str(0))"""
     
-script = "{"
+script = ""
 n = 0
 while n <= len(content)-1:
-    script = script + "\"name\" : \"" + (str(content["name"][n])) + "\","
-    script = script + "\"type\" : \"" + (str(content["type"][n])) + "\","
-    script = script + "\"group\" : \"" + (str(content["group"][n])) + "\","
+    script = script + "{\"name\" : \"" + (str(content["name"][n])) + "\","
     script = script + "\"x\" : \"" + (str(content["x"][n])) + "\","
     script = script + "\"y\" : \"" + (str(content["y"][n])) + "\","
     script = script + "\"z\" : \"" + (str(content["z"][n])) + "\","
+    script = script + "\"pointer_type\" : \"" + (str(content["type"][n])) + "\"}"
+
     n+=1
-script = script + "}"
+display(content)
 print(script)
